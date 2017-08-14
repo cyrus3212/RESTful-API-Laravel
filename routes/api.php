f926fcdcda5cc3f264d1e6d749adefcab89013e0
Anjo Tadena <anjo@coderepubliq.com>
2017-08-15 06:22:51 +0800

    Add resource route category, product, seller, transaction, and user.

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,20 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
+// Buyer
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+// Category
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+// Product
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+// Seller
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+// Transaction
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+// User
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
