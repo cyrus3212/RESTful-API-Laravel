f7415e4b26aafa47a27d20130c9d71c678430719
Anjo Tadena <anjo@coderepubliq.com>
2017-08-15 08:39:16 +0800

    Implement database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
+// Model
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+
+// DB facades
+use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +20,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Disable foreign key constraints
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $usersQuantity = 200;
+        $categoriesQuantity = 30;
+        $productsQuantity = 1000;
+        $transactionsQuantity = 1000;
+
+        factory(User::class, $usersQuantity)->create();
+        factory(Category::class, $categoriesQuantity)->create();
+        factory(Product::class, $productsQuantity)->create()->each(function ($product) {
+            $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+
+            $product->categories()->attach($categories);
+        });
+        factory(Transaction::class, $transactionsQuantity)->create();
     }
 }
