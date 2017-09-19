a03b125f2d2203fb6749878f30ea9ea19c17564e
Anjo Tadena <anjo@coderepubliq.com>
2017-09-20 07:28:12 +0800

    Add buyer category controller and implement view and api route.

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
 Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 
 // Category
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
