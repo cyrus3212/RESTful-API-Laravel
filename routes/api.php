0c6fd89dacb63a8db42da4b99f5079f50ff8b9dd
Anjo Tadena <anjo@coderepubliq.com>
2017-10-02 10:13:23 +0800

    Add product category.

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 
 // Sellerp
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
