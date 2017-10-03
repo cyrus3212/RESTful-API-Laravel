74afaf838f7bfd7456c44314f18dead8d1db3f11
Anjo Tadena <anjo@coderepubliq.com>
2017-10-04 06:17:43 +0800

    Implement delete product category.

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -85,8 +85,16 @@ class ProductCategoryController extends ApiController
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product, Category $category)
     {
+        if ($product->categories()->find($category->id)) {
+
+            $product->categories()->detach([$category->id]);
+
+            return $this->showAll($product->categories);
+        }
+
+        return $this->errorResponse('The specified category is not a category of this product.', 404);
         //
     }
 }
