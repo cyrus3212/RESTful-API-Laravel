f86fbb9da4c4d61e1a5b65be8fadcaba91eed3f3
Anjo Tadena <anjo@coderepubliq.com>
2017-10-13 21:28:39 +0800

    Implement auto update product if the products quantity is 0 and available will be set status to unavailable.' '

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,6 +18,14 @@ class AppServiceProvider extends ServiceProvider
         // Common string length issue
         // Set default in migration string length
         Schema::defaultStringLength(191);
+
+        Product::updated( function($product) {
+            if ($product->quantity === 0 && $product->isAvailable()) {
+                $product->status = Product::UNAVAILABLE_PRODUCT;
+
+                $product->save();
+            }
+        });
     }
 
     /**
