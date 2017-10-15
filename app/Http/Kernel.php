0b6b31f16b002132ac85b546f853fae8f65ec7e5
Anjo Tadena <anjo@coderepubliq.com>
2017-10-15 09:29:12 +0800

    Create custom throttle request middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:5,1',
             'bindings',
         ],
     ];
@@ -55,6 +55,8 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // Customer Throttle
+         'throttle' => \App\Http\Middleware\CustomThrottleRequests::class,
     ];
 }
---
+++ app/Http/Middleware/CustomThrottleRequests.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Traits\ApiResponser;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+
+// use Closure;
+
+class CustomThrottleRequests extends ThrottleRequests
+{
+    use ApiResponser;
+    /**
+     * Create a 'too many attempts' response.
+     *
+     * @param  string  $key
+     * @param  int  $maxAttempts
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function buildResponse($key, $maxAttempts)
+    {
+
+        $response =  $this->errorResponse('Too Many Attempts.', 429); //new Response('Too Many Attempts.', 429);
+
+        $retryAfter = $this->limiter->availableIn($key);
+
+        return $this->addHeaders(
+            $response, $maxAttempts,
+            $this->calculateRemainingAttempts($key, $maxAttempts, $retryAfter),
+            $retryAfter
+        );
+    }
+}
