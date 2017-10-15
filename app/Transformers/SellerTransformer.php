0bf5472e6aa33d07809cd14904192687f4b25ae6
Anjo Tadena <anjo@coderepubliq.com>
2017-10-15 10:26:51 +0800

    Implement Seller transformer.

---
+++ app/Transformers/SellerTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Seller;
+use League\Fractal\TransformerAbstract;
+
+class SellerTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Seller $seller)
+    {
+        return [
+            'id' => (int) $seller->id,
+            'name' => (string) $seller->name,
+            'email' => (string) $seller->email,
+            'isVerified' => (int) $seller->isVerified,
+            'creationDate' => $seller->created_at,
+            'lastChange' => $seller->updated_at,
+            'deletedDate' => isset($seller->deleted_at) ? (string)$seller->deleted_at : null
+        ];
+    }
+}
